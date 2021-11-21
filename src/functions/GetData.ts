1e0f46fb0e9962a5ecaff2746c9706e77e3f5d88
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-11-21 16:09:35 -0600

    include node_modules in build

--- src/functions/GetData.ts
+++ src/functions/GetData.ts
@@ -1,12 +1,15 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import has from 'lodash/fp/has';
 
 const generateRandomNumber = (min: number, max: number): number =>
   Math.floor(Math.random() * (max - min) + min);
 
-export const handler: APIGatewayProxyHandler = async () => {
+export const handler: APIGatewayProxyHandler = async (event) => {
   const min = 1;
   const max = 100;
   const randomNumber = generateRandomNumber(min, max);
+
+  console.log('has headers?: ', has('headers', event));
 
   return {
     statusCode: 200,
