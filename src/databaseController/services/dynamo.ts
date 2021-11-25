c15dac2a15b50d814d3698da0cc5e044b6818b4a
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-11-24 22:00:13 -0600

    remove try/catch block

--- src/databaseController/services/dynamo.ts
+++ src/databaseController/services/dynamo.ts
@@ -11,54 +11,44 @@ const dynamo = new DynamoDB.DocumentClient();
 const TABLE_NAME = process.env.DB_TABLE_NAME || 'Sample_Database';
 
 export const getAllItems = async () => {
-  try {
-    console.log(`starting scan on the ${TABLE_NAME} database table...`);
+  console.log(`starting scan on the ${TABLE_NAME} database table...`);
 
-    let data = await dynamo.scan({ TableName: TABLE_NAME }).promise();
-    let databaseItems = [...data.Items];
+  let data = await dynamo.scan({ TableName: TABLE_NAME }).promise();
+  let databaseItems = [...data.Items];
 
-    // scan maxes out at 1MB of data. continue scanning if there is more data to read
-    while (typeof data.LastEvaluatedKey !== 'undefined') {
-      console.log('Scanning for more...');
+  // scan maxes out at 1MB of data. continue scanning if there is more data to read
+  while (typeof data.LastEvaluatedKey !== 'undefined') {
+    console.log('Scanning for more...');
 
-      const params = {
-        TableName: TABLE_NAME,
-        ExclusiveStartKey: data.LastEvaluatedKey,
-      };
+    const params = {
+      TableName: TABLE_NAME,
+      ExclusiveStartKey: data.LastEvaluatedKey,
+    };
 
-      data = await dynamo.scan(params).promise();
-      databaseItems = [...databaseItems, ...data.Items];
-    }
-
-    console.log(
-      `Finished scanning all database items from the ${TABLE_NAME} table.`
-    );
-    console.log(`Scanned ${databaseItems.length} items.`);
-
-    return databaseItems;
-  } catch (err) {
-    console.log('An error occurred: ', JSON.stringify(err));
-    return [];
+    data = await dynamo.scan(params).promise();
+    databaseItems = [...databaseItems, ...data.Items];
   }
+
+  console.log(
+    `Finished scanning all database items from the ${TABLE_NAME} table.`
+  );
+  console.log(`Scanned ${databaseItems.length} items.`);
+
+  return databaseItems;
 };
 
 export const createNewItem = async (data: any) => {
-  try {
-    const id = uuidv4();
+  const id = uuidv4();
 
-    const params: DynamoPutItem = {
-      TableName: TABLE_NAME,
-      Item: {
-        id: id,
-        ...data,
-      },
-    };
-    console.log('item for creation: ', JSON.stringify(params));
-    await dynamo.put(params).promise();
+  const params: DynamoPutItem = {
+    TableName: TABLE_NAME,
+    Item: {
+      id: id,
+      ...data,
+    },
+  };
+  console.log('item for creation: ', JSON.stringify(params));
+  await dynamo.put(params).promise();
 
-    return params.Item;
-  } catch (err) {
-    console.log('An error occurred: ', JSON.stringify(err));
-    return {};
-  }
+  return params.Item;
 };
