8e68b65c9e57de865b5e81b1433df8561d85125d
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-11-24 21:42:27 -0600

    surround in try catch

--- src/databaseController/lambdas/getAllItems.ts
+++ src/databaseController/lambdas/getAllItems.ts
@@ -3,15 +3,23 @@ import has from 'lodash/fp/has';
 import { getAllItems } from '../services/dynamo';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-  console.log('getAllData event: ', JSON.stringify(event));
+  try {
+    console.log('getAllData event: ', JSON.stringify(event));
 
-  console.log('has headers?: ', has('headers', event));
+    console.log('has headers?: ', has('headers', event));
 
-  const allItems = await getAllItems();
-  console.log('allItems: ', JSON.stringify(allItems));
+    const allItems = await getAllItems();
+    console.log('allItems: ', JSON.stringify(allItems));
 
-  return {
-    statusCode: 200,
-    body: `hello`,
-  };
+    return {
+      statusCode: 200,
+      body: `hello`,
+    };
+  } catch (err) {
+    console.log('Error in handler: ', JSON.stringify(err));
+    return {
+      statusCode: 500,
+      body: '',
+    };
+  }
 };
